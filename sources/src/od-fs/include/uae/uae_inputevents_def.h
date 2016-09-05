a9539b020a4049591d434d614f2c468db345463a
johnray <none@none.com>
2016-09-05 13:20:21 +0200

    Fix uae_inputevents_def.h

--- sources/src/od-fs/include/uae/uae_inputevents_def.h
+++ sources/src/od-fs/include/uae/uae_inputevents_def.h
@@ -1,6 +1,6 @@
 #ifndef LIBAMIGA_LIBAMIGA_INPUT_EVENTS_H_
 #define LIBAMIGA_LIBAMIGA_INPUT_EVENTS_H_
 
-#include "../../../inputevents.def"
+#include "../inputevents.def"
 
 #endif // LIBAMIGA_LIBAMIGA_INPUT_EVENTS_H_
